d490e943ed6fa7b6d2d0a0325586556b628b3192
yinkar <hkrabs@yandex.com>
2023-03-03 01:20:29 +0300

    PWA support

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+    onNeedRefresh() {},
+    onOfflineReady() {},
+});
+
+if (typeof window !== 'undefined') {
+    import('./pwa');
+}
+
 createApp(App).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,44 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ilkkanmatik',
   plugins: [
-    vue()
+    vue(),
+    VitePWA({
+      workbox: {
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
+      },
+      includeAssets: [
+        'favicon.png',
+        'robots.txt',
+        'icon-192.png',
+        'icon-512.png',
+      ],
+      manifest: {
+          "name": "Ilkkanmatik",
+          "short_name": "Ilkkanmatik",
+          "description": "Random idioms of İlkkan from Gibi series",
+          "theme_color": "#ffbb00",
+          "background_color": "#3c3c3d",
+          "icons": [
+              {
+                  "src": "icon-192.png",
+                  "sizes": "192x192",
+                  "type": "image/png",
+                  "purpose": "any maskable",
+              },
+              {
+                  "src": "icon-512.png",
+                  "sizes": "512x512",
+                  "type": "image/png",
+                  "purpose": "any maskable",
+              }
+          ],
+      },
+    }),
   ],
 });
